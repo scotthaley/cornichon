f44bc5cd4996fd90b86d70eb8b62442961bc1435
Sctt Haley <williamscotthaley@gmail.com>
2017-04-12 23:03:26 -0500

    Remove copy file stuff (didn't work)

--- cucumber.js
+++ cucumber.js
@@ -140,26 +140,13 @@ module.exports = (() => {
   const getSupportCode = (cli, features) => {
     return co(function* () {
       const configuration = yield cli.getConfiguration()
-      let supportCodePaths = []
-      fse.emptyDirSync(path.join(__dirname, 'temp/'))
-      for (let i in configuration.supportCodePaths) {
-        let realPath = configuration.supportCodePaths[i]
-        let tempPath = path.join(__dirname, 'temp/', realPath.replace(/^.*\\features\\/, 'features\\'))
-        fse.copySync(realPath, tempPath)
-        let data = fs.readFileSync(tempPath, 'utf8')
-        let fixedData = data.replace(/'\.+\/.*'|"\.+\/.*"|`\.+\/.*`/g, function (match) {
-          let quotes = match.charAt(0)
-          let fixedPath = path.join(realPath.substring(0, realPath.lastIndexOf('\\')), match.replace(/['"`]/g, ''))
-          return `${quotes}${fixedPath.replace(/\\/g, '/')}${quotes}`
-        })
-        fs.writeFileSync(tempPath, fixedData)
-        supportCodePaths.push(tempPath)
-      }
-      let supportCode = cli.getSupportCodeLibrary(supportCodePaths)
+      let supportCode = cli.getSupportCodeLibrary(configuration.supportCodePaths)
       let supportCodeMapped = []
       for (let i in supportCode.stepDefinitions) {
         let stepDef = supportCode.stepDefinitions[i]
         stepDef.expression = new cucumberExpression.CucumberExpression(stepDef.pattern, [], supportCode.parameterTypeRegistry)
+        stepDef.uri = stepDef.uri.replace(/^.*\\features\\/, 'features\\')
+        stepDef.uri_full = stepDef.uri
         stepDef.code = beautify(stepDef.code.toString(), {indent_size: 4})
         // needs to happen after stepDef.code is set, before stripping ID
         stepDef.cornichonID = cucumberHelper.getStepID(stepDef)
@@ -168,8 +155,6 @@ module.exports = (() => {
         stepDef.code = stepDef.code.replace(/ ?{cornichon: [0-9]+}/, '')
         stepDef.keyword = cucumberHelper.getStepKeyword(stepDef)
         stepDef.usage = cornichon.getUsage(stepDef.cornichonID) || 'No usage information provided.'
-        stepDef.uri = stepDef.uri.replace(/^.*\\features\\/, 'features\\')
-        stepDef.uri_full = stepDef.uri
         stepDef.features = []
         stepDef.scenarios = []
         for (let f in features) {
