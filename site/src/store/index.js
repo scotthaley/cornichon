f23c33e4ae6b03f9ee1eebcb19f01e6a15e6c7ea
Sctt Haley <williamscotthaley@gmail.com>
2017-04-16 13:45:24 -0500

    Add scenarios to store

--- site/src/store/index.js
+++ site/src/store/index.js
@@ -7,19 +7,26 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     settings: ['1', '2', '3'],
-    tags: []
+    tags: [],
+    scenarios: []
   },
   mutations: {
     SET_TAGS (state, res) {
       state.tags = res
+    },
+    ADD_SCENARIO (state, scenario) {
+      state.scenarios.push(scenario)
     }
   },
   actions: {
-    TAGS (state) {
+    TAGS () {
       app.fetch('tags')
         .done(function (res) {
           store.commit('SET_TAGS', res)
         })
+    },
+    QUEUE_SCENARIO ({ commit }, scenario) {
+      commit('ADD_SCENARIO', scenario)
     }
   }
 })
